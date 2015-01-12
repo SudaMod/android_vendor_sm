8cce986fa00324212346acb29e6a97df391531cc
ghbhaha <ghbhaha@gmail.com>
2015-01-12 18:39:13 +0800

    SudaMod bringup
    Change-Id: I19b0c0abba3e72a71ee644a91c8fe17ce1f41a25

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,16 +1,16 @@
-# Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
+# Inherit common SM stuff
+$(call inherit-product, vendor/sm/config/common.mk)
 
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
-# Include CM audio files
-include vendor/cm/config/cm_audio.mk
+# Include SM audio files
+include vendor/sm/config/sm_audio.mk
 
-# Include CM LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
+# Include SM LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/sm/overlay/dictionaries
 
-# Optional CM packages
+# Optional SM packages
 PRODUCT_PACKAGES += \
     Galaxy4 \
     HoloSpiralWallpaper \
@@ -32,7 +32,7 @@ PRODUCT_PACKAGES += \
     libvideoeditor_videofilters \
     libvideoeditorplayer
 
-# Extra tools in CM
+# Extra tools in SM
 PRODUCT_PACKAGES += \
     vim \
     zip \
