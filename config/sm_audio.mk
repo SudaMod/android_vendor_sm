ea2b756e76bb42d08d829c078e0d7a4e4d569494
Haibo Gu <ghbhaha@users.noreply.github.com>
2015-10-10 15:29:29 +0800

    Update sm_audio.mk

--- config/sm_audio.mk
+++ config/sm_audio.mk
@@ -9,34 +9,87 @@ UI_PATH := vendor/sm/prebuilt/common/media/audio/ui
 
 # Alarms
 PRODUCT_COPY_FILES += \
-	$(ALARM_PATH)/CyanAlarm.ogg:system/media/audio/alarms/CyanAlarm.ogg \
-	$(ALARM_PATH)/NuclearLaunch.ogg:system/media/audio/alarms/NuclearLaunch.ogg
+    $(ALARM_PATH)/Argon.ogg:system/media/audio/alarms/Argon.ogg \
+    $(ALARM_PATH)/Carbon.ogg:system/media/audio/alarms/Carbon.ogg \
+    $(ALARM_PATH)/Helium.ogg:system/media/audio/alarms/Helium.ogg \
+    $(ALARM_PATH)/Krypton.ogg:system/media/audio/alarms/Krypton.ogg \
+    $(ALARM_PATH)/Neon.ogg:system/media/audio/alarms/Neon.ogg \
+    $(ALARM_PATH)/Osmium.ogg:system/media/audio/alarms/Osmium.ogg \
+    $(ALARM_PATH)/Oxygen.ogg:system/media/audio/alarms/Oxygen.ogg \
+    $(ALARM_PATH)/Platinum.ogg:system/media/audio/alarms/Platinum.ogg
 
 # Notifications
 PRODUCT_COPY_FILES += \
-	$(NOTIFICATION_PATH)/CyanDoink.ogg:system/media/audio/notifications/CyanDoink.ogg \
-	$(NOTIFICATION_PATH)/CyanMail.ogg:system/media/audio/notifications/CyanMail.ogg \
-	$(NOTIFICATION_PATH)/CyanMessage.ogg:system/media/audio/notifications/CyanMessage.ogg \
-	$(NOTIFICATION_PATH)/Laser.ogg:system/media/audio/notifications/Laser.ogg \
-	$(NOTIFICATION_PATH)/Naughty.ogg:system/media/audio/notifications/Naughty.ogg \
-	$(NOTIFICATION_PATH)/Pong.ogg:system/media/audio/notifications/Pong.ogg \
-	$(NOTIFICATION_PATH)/Rang.ogg:system/media/audio/notifications/Rang.ogg \
-	$(NOTIFICATION_PATH)/Stone.ogg:system/media/audio/notifications/Stone.ogg
+    $(NOTIFICATION_PATH)/Ariel.ogg:system/media/audio/notifications/Ariel.ogg \
+    $(NOTIFICATION_PATH)/Blip.ogg:system/media/audio/notifications/Blip.ogg \
+    $(NOTIFICATION_PATH)/Carme.ogg:system/media/audio/notifications/Carme.ogg \
+    $(NOTIFICATION_PATH)/Ceres.ogg:system/media/audio/notifications/Ceres.ogg \
+    $(NOTIFICATION_PATH)/Chime.ogg:system/media/audio/notifications/Chime.ogg \
+    $(NOTIFICATION_PATH)/Elara.ogg:system/media/audio/notifications/Elara.ogg \
+    $(NOTIFICATION_PATH)/Element.ogg:system/media/audio/notifications/Element.ogg \
+    $(NOTIFICATION_PATH)/Europa.ogg:system/media/audio/notifications/Europa.ogg \
+    $(NOTIFICATION_PATH)/Hello.ogg:system/media/audio/notifications/Hello.ogg \
+    $(NOTIFICATION_PATH)/Iapetus.ogg:system/media/audio/notifications/Iapetus.ogg \
+    $(NOTIFICATION_PATH)/Organic.ogg:system/media/audio/notifications/Organic.ogg \
+    $(NOTIFICATION_PATH)/Rhea.ogg:system/media/audio/notifications/Rhea.ogg \
+    $(NOTIFICATION_PATH)/Salacia.ogg:system/media/audio/notifications/Salacia.ogg \
+    $(NOTIFICATION_PATH)/Tethys.ogg:system/media/audio/notifications/Tethys.ogg \
+    $(NOTIFICATION_PATH)/Titan.ogg:system/media/audio/notifications/Titan.ogg
 
 # Ringtones
 ifeq ($(TARGET_NEEDS_BOOSTED_SOUNDS),true)
 PRODUCT_COPY_FILES += \
-	$(RINGTONE_PATH)/boosted/Boxbeat.ogg:system/media/audio/ringtones/Boxbeat.ogg \
-	$(RINGTONE_PATH)/boosted/CyanTone.ogg:system/media/audio/ringtones/CyanTone.ogg \
-	$(RINGTONE_PATH)/boosted/Highscore.ogg:system/media/audio/ringtones/Highscore.ogg \
-	$(RINGTONE_PATH)/boosted/Lyon.ogg:system/media/audio/ringtones/Lyon.ogg \
-	$(RINGTONE_PATH)/boosted/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg
+    $(RINGTONE_PATH)/boosted/Boxbeat.ogg:system/media/audio/ringtones/Boxbeat.ogg \
+    $(RINGTONE_PATH)/boosted/CyanTone.ogg:system/media/audio/ringtones/CyanTone.ogg \
+    $(RINGTONE_PATH)/boosted/Highscore.ogg:system/media/audio/ringtones/Highscore.ogg \
+    $(RINGTONE_PATH)/boosted/Lyon.ogg:system/media/audio/ringtones/Lyon.ogg \
+    $(RINGTONE_PATH)/boosted/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg
 else
 PRODUCT_COPY_FILES += \
-	$(RINGTONE_PATH)/Boxbeat.ogg:system/media/audio/ringtones/Boxbeat.ogg \
-	$(RINGTONE_PATH)/CyanTone.ogg:system/media/audio/ringtones/CyanTone.ogg \
-	$(RINGTONE_PATH)/Highscore.ogg:system/media/audio/ringtones/Highscore.ogg \
-	$(RINGTONE_PATH)/Lyon.ogg:system/media/audio/ringtones/Lyon.ogg \
-	$(RINGTONE_PATH)/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg \
-	$(RINGTONE_PATH)/Sheep.mp3:system/media/audio/ringtones/Sheep.mp3
+    $(RINGTONE_PATH)/Awake.ogg:system/media/audio/ringtones/Awake.ogg \
+    $(RINGTONE_PATH)/Callisto.ogg:system/media/audio/ringtones/Callisto.ogg \
+    $(RINGTONE_PATH)/Complex.ogg:system/media/audio/ringtones/Complex.ogg \
+    $(RINGTONE_PATH)/Ganymede.ogg:system/media/audio/ringtones/Ganymede.ogg \
+    $(RINGTONE_PATH)/Hello.ogg:system/media/audio/ringtones/Hello.ogg \
+    $(RINGTONE_PATH)/Keys.ogg:system/media/audio/ringtones/Keys.ogg \
+    $(RINGTONE_PATH)/Luna.ogg:system/media/audio/ringtones/Luna.ogg \
+    $(RINGTONE_PATH)/Momentum.ogg:system/media/audio/ringtones/Momentum.ogg \
+    $(RINGTONE_PATH)/Natural.ogg:system/media/audio/ringtones/Natural.ogg \
+    $(RINGTONE_PATH)/Onward.ogg:system/media/audio/ringtones/Onward.ogg \
+    $(RINGTONE_PATH)/Organic.ogg:system/media/audio/ringtones/Organic.ogg \
+    $(RINGTONE_PATH)/Phobos.ogg:system/media/audio/ringtones/Phobos.ogg \
+    $(RINGTONE_PATH)/RobotsforEveryone.ogg:system/media/audio/ringtones/RobotsforEveryone.ogg \
+    $(RINGTONE_PATH)/Sedna.ogg:system/media/audio/ringtones/Sedna.ogg \
+    $(RINGTONE_PATH)/Simple.ogg:system/media/audio/ringtones/Simple.ogg \
+    $(RINGTONE_PATH)/Strings.ogg:system/media/audio/ringtones/Strings.ogg \
+    $(RINGTONE_PATH)/Titania.ogg:system/media/audio/ringtones/Titania.ogg \
+    $(RINGTONE_PATH)/Triton.ogg:system/media/audio/ringtones/Triton.ogg \
+    $(RINGTONE_PATH)/Umbriel.ogg:system/media/audio/ringtones/Umbriel.ogg
 endif
+
+# UI
+PRODUCT_COPY_FILES += \
+    $(UI_PATH)/audio_end.ogg:system/media/audio/ui/audio_end.ogg \
+    $(UI_PATH)/audio_initiate.ogg:system/media/audio/ui/audio_initiate.ogg \
+    $(UI_PATH)/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
+    $(UI_PATH)/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
+    $(UI_PATH)/Dock.ogg:system/media/audio/ui/Dock.ogg \
+    $(UI_PATH)/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
+    $(UI_PATH)/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
+    $(UI_PATH)/KeypressInvalid.ogg:system/media/audio/ui/KeypressInvalid.ogg \
+    $(UI_PATH)/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
+    $(UI_PATH)/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg \
+    $(UI_PATH)/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
+    $(UI_PATH)/Lock.ogg:system/media/audio/ui/Lock.ogg \
+    $(UI_PATH)/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
+    $(UI_PATH)/NFCFailure.ogg:system/media/audio/ui/NFCFailure.ogg \
+    $(UI_PATH)/NFCInitiated.ogg:system/media/audio/ui/NFCInitiated.ogg \
+    $(UI_PATH)/NFCSuccess.ogg:system/media/audio/ui/NFCSuccess.ogg \
+    $(UI_PATH)/NFCTransferComplete.ogg:system/media/audio/ui/NFCTransferComplete.ogg \
+    $(UI_PATH)/NFCTransferInitiated.ogg:system/media/audio/ui/NFCTransferInitiated.ogg \
+    $(UI_PATH)/Trusted.ogg:system/media/audio/ui/Trusted.ogg \
+    $(UI_PATH)/Undock.ogg:system/media/audio/ui/Undock.ogg \
+    $(UI_PATH)/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
+    $(UI_PATH)/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
+    $(UI_PATH)/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
+    $(UI_PATH)/WirelessChargingStarted.ogg:system/media/audio/ui/WirelessChargingStarted.ogg
