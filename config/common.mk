e3d7cec64e885adf8a161cf3e09e607c4bbb5ea5
ghbhaha <ghbhaha@gmail.com>
2016-02-03 18:34:13 +0800

    Merge branch 'cm-13.0' into sm-2.0
    Change-Id: Ia1ac5250a832467dbbcdb11498f98c18588d44fc

--- config/common.mk
+++ config/common.mk
@@ -135,7 +135,7 @@ PRODUCT_COPY_FILES +=  \
 
 # Live lockscreen
 PRODUCT_COPY_FILES += \
-    vendor/cm/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml
+    vendor/sm/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml
 
 # Theme engine
 include vendor/sm/config/themes_common.mk
