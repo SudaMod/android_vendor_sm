b67c495ac5b7d01da279f8875299d7effdc5f897
ghbhaha <ghbhaha@gmail.com>
2016-06-14 12:07:11 +0800

    update
    Change-Id: I0f9f264283833c3fa6111827d1b828f093da7aee

--- config/common_mini_tv.mk
+++ config/common_mini_tv.mk
@@ -1,9 +1,5 @@
-# Inherit common CM stuff
+# Inherit common SM stuff
 $(call inherit-product, vendor/sm/config/common_mini.mk)
-
-# Required SM packages
-PRODUCT_PACKAGES += \
-    LatinIME
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
