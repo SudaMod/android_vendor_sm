e6b41fd86cb95788be9939cecdec81745e3d108f
Haibo Gu <ghbhaha@users.noreply.github.com>
2016-10-16 11:54:04 +0800

    Update common_full_tablet_wifionly.mk

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -8,14 +8,6 @@ PRODUCT_PACKAGES += \
 # Include SM LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/sm/overlay/dictionaries
 
-<<<<<<< HEAD
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Hello.ogg \
-    ro.config.alarm_alert=Helium.ogg
-
-=======
->>>>>>> 0432d1c84921af05b0f32540e30fd1d793662322
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/sm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
