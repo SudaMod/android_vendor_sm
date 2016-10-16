1808fc0ff71f32e5227e9fdffb0d0def6a12ddb4
ghbhaha <ghbhaha@gmail.com>
2016-10-16 11:57:44 +0800

    update
    Change-Id: I8a6fb01269b0068f81fd16f9db9a84bd1e084c06

--- config/common.mk
+++ config/common.mk
@@ -287,6 +287,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
+
+DEVICE_PACKAGE_OVERLAYS += vendor/sm/overlay/common
 else
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=0
