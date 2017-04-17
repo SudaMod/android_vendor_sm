bd2b8659ea62ac77e782bb175d5b1edba8bb3741
Haibo Gu <ghbhaha@users.noreply.github.com>
2017-04-17 14:07:59 +0800

    Update backuptool.sh

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -5,7 +5,7 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V=SM71.2
+export V=71.2
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
