d943cfa491ac56b0903e4a4d50713948b474da92
Haibo Gu <ghbhaha@users.noreply.github.com>
2017-08-25 20:08:41 +0800

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -86,10 +86,6 @@ PRODUCT_COPY_FILES += $(shell test -d vendor/sm/prebuilt/google/app/GooglePinYin
     find vendor/sm/prebuilt/google/app/GooglePinYin -name '*.so' \
     -printf '%p:system/app/GooglePinYin/lib/arm/%f ')
 
-#BusyBox
-PRODUCT_COPY_FILES += \
-    vendor/sm/prebuilt/bb/bb.apk:system/app/bb/bb.apk
-
 #Kernel
 PRODUCT_COPY_FILES += \
     vendor/sm/prebuilt/Kernel/kernel.apk:system/app/Kernel/kernel.apk
