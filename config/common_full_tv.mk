38366583ffa3ad9363a6f2071c26e46005cead87
aromer <Aromer-room@users.noreply.github.com>
2017-11-18 16:16:22 +0800

    Update common_full_tv.mk

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -3,4 +3,4 @@ $(call inherit-product, vendor/sm/config/common_full.mk)
 
 PRODUCT_PACKAGES += TvSettings
 
-DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/tv
+DEVICE_PACKAGE_OVERLAYS += vendor/sm/overlay/tv
